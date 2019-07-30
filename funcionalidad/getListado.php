6cf4a3fff116eb6c26915a8705c18c9fa0d9a910
Devilzac <shane199274@gmail.com>
2019-07-30 14:03:19 +0200

    anadir getlistadophp

---
+++ funcionalidad/getListado.php
@@ -0,0 +1,14 @@
+<?php
+$coders = array("Alexandro", "Amau", "Camilo",
+                "Carlos", "Delfina","Enric", "Erik","Ferran",
+                "Franco", "Janina","Jason", "Kavan", "Manuela",
+                "Nathalia", "Nellay", "Noelia", "Nuria", "Raul",
+                "Sergi", "Shuangjie", "Solomon", "Unai","Andres",
+                "Thierry");
+$fomadores = array("Edgar","David","Jennifer","Marina", "Ali");
+
+foreach ($coders as $value) {
+   echo $value;
+}
+
+?>
